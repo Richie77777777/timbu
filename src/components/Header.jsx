88d93e17bf5748b2173a4298f3d8c3e985489192
iBabs <ibrahimbabalola8@gmail.com>
2024-07-22 16:36:49 +0100

    gugugaga

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
         <header>
            
             <Nav/>
-            <div className="h-48  md:h-96 w-full flex justify-start -z-30 text-brandbrown font-serif relative"
+            <div className="h-48  md:h-96 w-full flex justify-start -z-30 text-white font-serif relative"
                 style={{
                     backgroundImage: "url(/assets/wine_glass.webp)",
                     backgroundSize: "cover",
@@ -18,7 +18,7 @@ function Header() {
                 }}
             >
                 <div className="w-1/2 flex flex-col absolute left-4 bottom-5 py-10">
-                {/* <img src="/assets/Frame 13.png" alt="logo" className='md:h-[100px]' /> */}
+               
                     
                     <p className="md:text-4xl md:w-96 md:leading-tight w-full">Elevate your wine experience</p>
                 </div>
